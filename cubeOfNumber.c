74d3c7651bf8372c7e3b7261ce88a9ae82adbcb3
Vatsal Tripathi <vatsal.tiwari302005@gmail.com>
2024-07-17 17:46:33 +0530

    Add files via upload

---
+++ cubeOfNumber.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+
+//Code for finding cube of a number
+
+int main() {
+    int a;
+    printf("Enter number = ");
+    scanf("%d", &a);
+
+    printf("Cube of number = %d", a*a*a);
+    return 0;
+}
