2617bff4e09408310ddaf0e12b6280902dc4b6c5
Vatsal Tripathi <vatsal.tiwari302005@gmail.com>
2024-07-17 17:18:51 +0530

    Add files via upload

---
+++ sumOfThreeNumbers.c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+
+// code for sum of three nubers
+
+int main() {
+   int a, b, c;
+
+   printf("Sum of three numbers.\n");
+
+   printf("enter first number = ");
+   scanf("%d", &a);
+
+   printf("enter second number = ");
+   scanf("%d", &b);
+
+   printf("enter third number = ");
+   scanf("%d", &c);
+   
+   printf("result = %d", a + b + c);
+   return 0;
+   
+}
